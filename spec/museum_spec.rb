91e5437eb81ba64e526ae04f8ce69bdd6905d336
Erik Riggs <riggs.erik@gmail.com>
2022-07-27 09:04:25 -0600

    Test: Museum class has attributes

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe Museum do
 
   it 'does exist' do
     expect(@dmns).to be_instance_of(Museum)
-  end 
+  end
+
+  it "has attributes" do
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(@dmns.exhibits).to eq([])
+  end
 end
